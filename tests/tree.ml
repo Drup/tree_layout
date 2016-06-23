94abc4b4a51d2eff7b88ce296e4ac2cd3fdc167b
Drup <drupyog@zoho.com>
2016-06-23 16:36:56 +0200

    Use n-ary trees as tests and fix for last tyxml.

--- tests/tree.ml
+++ tests/tree.ml
@@ -1,6 +1,10 @@
 type info = {label : int ; w : float ; h : float }
 
-type tree = Node of tree * info * tree | Leaf of info
+type tree = Node of info * tree array | Leaf of info
+
+let array_rev_iter f a =
+  let last = Array.length a - 1 in
+  for i = last downto 0 do f a.(i) done
 
 module T = struct
 
@@ -13,23 +17,23 @@ module T = struct
 
   let children () v k = match v with
     | Leaf _ -> ()
-    | Node (t1, _, t2) -> k t1 ; k t2
+    | Node (_, a) -> Array.iter k a
 
   let rev_children () v k = match v with
     | Leaf _ -> ()
-    | Node (t1, _, t2) -> k t2 ; k t1
+    | Node (_, a) -> array_rev_iter k a
 
   let leftmost_child () = function
     | Leaf _ -> None
-    | Node (t, _, _) -> Some t
+    | Node (_, a) -> Some a.(0)
 
   let rightmost_child () = function
     | Leaf _ -> None
-    | Node (_, _, t) -> Some t
+    | Node (_, a) -> Some a.(Array.length a - 1)
 
   let is_parent () ~parent ~child = match parent with
     | Leaf _ -> false
-    | Node (t1, _, t2) -> V.equal t1 child || V.equal t2 child
+    | Node (_, a) -> Array.exists (V.equal child) a
 
 end
 
@@ -42,29 +46,30 @@ module Gen = struct
 
   let node, leaf =
     let r = ref 0 in
-    let node t1 t2 = incr r ; Node (t1, label !r, t2) in
+    let node a = incr r ; Node (label !r, a) in
     let leaf () = incr r ; Leaf (label !r) in
     node, leaf
 
   let rec make n =
     if n <= 1 then leaf ()
     else
-      let i = 1 + Random.int (n-1) in
-      node (make i) (make (n-i))
+      let l = 1 + Random.int (min 4 (n-1)) in
+      let a = Array.init l (fun _ -> make ((n-1)/l)) in
+      node a
 
 end
 
-let width (Leaf info | Node (_,info,_)) = info.w
+let width (Leaf info | Node (info,_)) = info.w
 let distance l1 l2 = width l1 /. 2. +. 0.2 +. width l2 /. 2.
 
 module Output = struct
-  open Svg
+  module M = Tyxml.Svg
   open Tree_layout
 
   let line p1 p2 =
     M.(path ~a:[
         a_d @@ Printf.sprintf {|M%f,%f L%f,%f|} p1.x p1.y p2.x p2.y ;
-        a_stroke (`Color ("black", None)) ; a_strokewidth (0.06, None) ;
+        a_stroke (`Color ("black", None)) ; a_stroke_width (0.06, None) ;
         a_fill `None ;
       ] [])
 
@@ -73,39 +78,39 @@ module Output = struct
       rect ~a:[
         a_x (x -. info.w/.2., None) ; a_y (y -. info.h/.2., None) ;
         a_width (info.w, None) ; a_height (info.h, None) ;
-        a_strokewidth (0.03, None) ;
+        a_stroke_width (0.03, None) ;
         a_style "fill:#4860E8;stroke:black"
       ][] ;
       text ~a:[
         a_x_list [x, None] ; a_y_list [y, None] ;
-        a_text_anchor `Middle ; a_fontsize "0.4" ;
+        a_text_anchor `Middle ; a_font_size "0.4" ;
       ] [pcdata @@ string_of_int info.label] ;
     ]
+
+  let list_map_array f a = List.map f @@ Array.to_list a
+  let list_flatmap_array f a =
+    List.concat @@ list_map_array f a
 
   let rec svg_shapes h t =
     let pos = L.H.find h t in
     match t with
     | Leaf info -> rect info pos
-    | Node (t1, info, t2) ->
-      svg_shapes h t1 @
-        svg_shapes h t2 @
-        rect info pos
+    | Node (info, a) ->
+      list_flatmap_array (svg_shapes h) a @ rect info pos
 
   let rec svg_lines h t =
     let pos = L.H.find h t in
     match t with
     | Leaf _ -> []
-    | Node (t1, _, t2) ->
-      line pos (L.H.find h t1) ::
-        line pos (L.H.find h t2) ::
-        svg_lines h t1 @
-        svg_lines h t2
+    | Node (_, a) ->
+      list_map_array (fun x -> line pos @@ L.H.find h x) a
+      @ list_flatmap_array (svg_lines h) a
 
   let doc seed h t =
     let pos, size = L.boundaries ~margins:{x=1.;y=1.} h in
     M.(svg ~a:[
         a_width (1200., Some `Px) ; a_height (700., Some `Px) ;
-        a_viewbox (pos.x, pos.y, size.x, size.y) ;
+        a_viewBox (pos.x, pos.y, size.x, size.y) ;
       ] (
         title (pcdata @@ Printf.sprintf "Tree layout -- Seed: %i" seed)::
         svg_lines h t @ svg_shapes h t
@@ -116,10 +121,10 @@ let () =
   Random.self_init () ;
   let seed = Random.int (1 lsl 29) in
   Random.init seed ;
+  Printf.printf "Seed : %i\n" seed ;
 
-  let tree = Gen.make 50 in
+  let tree = Gen.make 100 in
   let h = L.layered ~distance () tree in
-  let file = open_out Sys.argv.(1) in
+  let file = Format.formatter_of_out_channel @@ open_out Sys.argv.(1) in
   let doc = Output.doc seed h tree in
-  Svg.P.print ~output:(output_string file) doc ;
-  close_out file
+  Format.fprintf file "%a@." (Tyxml.Svg.pp ()) doc
