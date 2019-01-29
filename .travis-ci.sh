b987bede5b92f7b62b4a737ab8b3bda5c81ef708
Drup <drupyog@zoho.com>
2019-01-29 20:10:18 +0100

    Adapt doc export to dune.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,8 +7,15 @@ set -e
 set +x
 set -o errexit -o nounset
 
+if [ -z "${DOC+x}" ]; then
+  echo "This is not a push Travis-ci build, doing nothing..."
+  exit 0
+else
+  echo "Building docs..."
+fi
+
 eval `opam config env`
-opam install tree_layout --with-test --deps-only -v
+opam install -y odoc
 make doc
 
 if [ -z "$TRAVIS" \
@@ -30,11 +37,10 @@ DOCDIR=.gh-pages
 git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} $DOCDIR 2>&1 | sed -e "s/$GH_TOKEN/!REDACTED!/g"
 git -C $DOCDIR checkout gh-pages || git -C $DOCDIR checkout --orphan gh-pages
 
-rm -rf $DOCDIR/dev/*
-cp _build/*.docdir/* $DOCDIR/dev
+cp -r _build/default/_doc/_html/* ${DOCDIR}/dev/
 
 git -C $DOCDIR config user.email "travis@travis-ci.org"
 git -C $DOCDIR config user.name "Travis"
-git -C $DOCDIR add --all dev
+git -C $DOCDIR add --all
 git -C $DOCDIR commit --allow-empty -m "Travis build $TRAVIS_BUILD_NUMBER pushed docs to gh-pages"
 git -C $DOCDIR push origin gh-pages 2>&1 | sed -e "s/$GH_TOKEN/!REDACTED!/g"
